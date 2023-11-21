884f53f04851c8ad239ac703c9bf20a16fc32028
thembones79 <michal.szumnarski@gmail.con>
2023-11-21 01:44:48 +0100

    finished

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,14 @@ const setupCanvas = () => {
 
     animate(ctx);
 };
+let playerState = "idle";
+const handleChange = () => {
+    const dropdown = document.getElementById("animations");
+    dropdown?.addEventListener("change", (e: Event) => {
+        const d = e.target as HTMLInputElement;
+        playerState = d.value;
+    });
+};
 const playerImage = new Image();
 playerImage.src = "assets/shadow_dog.png";
 const sw = 575;
@@ -21,10 +29,11 @@ const dy = 0;
 const dw = 575;
 const dh = 523;
 let fx = 0;
-let fy = 4;
+let fy = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
-const spriteAnimatios = [];
+type Frames = { loc: { x: number; y: number }[] };
+const spriteAnimatios: { [key: string]: Frames } = {};
 const animationStates = [
     {
         name: "idle",
@@ -34,21 +43,78 @@ const animationStates = [
         name: "jump",
         frames: 7,
     },
+    {
+        name: "fall",
+        frames: 7,
+    },
+    {
+        name: "run",
+        frames: 9,
+    },
+    {
+        name: "dizzy",
+        frames: 11,
+    },
+    {
+        name: "sit",
+        frames: 5,
+    },
+    {
+        name: "roll",
+        frames: 7,
+    },
+    {
+        name: "bite",
+        frames: 7,
+    },
+    {
+        name: "ko",
+        frames: 12,
+    },
+    {
+        name: "getHit",
+        frames: 4,
+    },
 ];
+
+animationStates.forEach((state, i) => {
+    let frames: Frames = {
+        loc: [],
+    };
+
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * sw;
+        let positionY = i * sh;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+    spriteAnimatios[state.name as keyof typeof spriteAnimatios] = frames;
+});
 
 function animate(ctx: CanvasRenderingContext2D) {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame / staggerFrames) % 11;
+    let position =
+        Math.floor(gameFrame / staggerFrames) %
+        spriteAnimatios[playerState].loc.length;
     fx = sw * position;
-    ctx.drawImage(playerImage, fx, fy * sh, sw, sh, dx, dy, dw, dh);
+    fy = spriteAnimatios[playerState].loc[0].y;
+    ctx.drawImage(playerImage, fx, fy, sw, sh, dx, dy, dw, dh);
     gameFrame++;
     requestAnimationFrame(() => animate(ctx));
 }
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-  <canvas id="canvas1"></canvas>
-  </div>
+<div>
+    <canvas id="canvas1"></canvas>
+    <div class="controls">
+        <label for="animations">Choose animation:</label>
+        <select id="animations" name="animations">
+        ${animationStates
+        .map(({ name }) => `<option value="${name}">${name}</option>`)
+        .join("")}
+        </select>
+    </div>
+</div>
 `;
 
 setupCanvas();
+handleChange();
